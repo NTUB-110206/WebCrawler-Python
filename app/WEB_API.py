22fffc289fe10adb4d75ba31a9c0c58aeec2ba33
Joe_mac <joelin890120@gmail.com>
2021-11-29 09:50:24 +0800

    feat: backend middleware
    post new news

--- app/WEB_API.py
+++ app/WEB_API.py
@@ -9,6 +9,13 @@ def get_newslist(news_website, limit):
     results = res.json()
     return results
 
+def post_newslist(newslist):
+    headers = {'Content-Type': 'application/json'}
+    payload = json.dumps({'news': newslist})
+    res = requests.post(backend_SERVERURL+'/newslist', headers=headers, data=payload)
+    results = res.json()
+    return results
+
 def get_newsapi(last_date):
     res = requests.get('https://newsapi.org/v2/everything?sources=bbc-news&q=bitcoin&from='+str(last_date)+'&sortBy=publishedAt&apiKey='+str(NEWSAPI_APIKEY))
     results = res.json()
