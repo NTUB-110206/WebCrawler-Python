ce4f58f09640d175af52a5ea7a99c204f4786131
Joe <joelin890120@gmail.com>
2021-12-14 03:11:43 +0800

    feat: add  utils for parse datetime

--- app/utils.py
+++ app/utils.py
@@ -2,14 +2,20 @@ import re
 import datetime
 import pytz
 def parseDatetime(date_str):
-
-    if str(date_str).find("sec") != -1:
+    date_str = str(date_str)
+    date_str = date_str[date_str.find('|')+2:] if date_str.find('|')>0 else date_str
+    if date_str.find("sec") != -1:
         date_str=datetime.datetime.now().astimezone(pytz.utc)-datetime.timedelta(seconds=int(re.sub('\D', '', date_str)))
-    elif str(date_str).find("min") != -1:
+    elif date_str.find("min") != -1:
         date_str=datetime.datetime.now().astimezone(pytz.utc)-datetime.timedelta(minutes=int(re.sub('\D', '', date_str)))
-    elif str(date_str).find("hour") != -1:
+    elif date_str.find("hour") != -1:
         date_str=datetime.datetime.now().astimezone(pytz.utc)-datetime.timedelta(hours=int(re.sub('\D', '', date_str)))
-    elif str(date_str).find("day") != -1:
+    elif date_str.find("day") != -1:
         date_str=datetime.datetime.now().astimezone(pytz.utc)-datetime.timedelta(days=int(re.sub('\D', '', date_str)))
-
+    else:
+        date_str = str(mdy_to_ymd(date_str.replace(" ", "")))+' 00:00:00'
+    print("parseDatetime",date_str)
     return date_str
+
+def mdy_to_ymd(d):
+    return datetime.datetime.strptime(d, '%b%d,%Y').strftime('%Y-%m-%d')
